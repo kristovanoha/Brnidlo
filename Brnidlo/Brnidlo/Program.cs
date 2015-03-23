93b58685d1748b1ccea11b9cb930aeff590e2ac9
kristovanoha <kristovanoha@centrum.cz>
2015-03-23 08:33:35 +0100

    posled

--- Brnidlo/Brnidlo/Program.cs
+++ Brnidlo/Brnidlo/Program.cs
@@ -31,7 +31,7 @@ namespace Brnidlo
         private static void testMetoda()
         {
             Console.WriteLine("Prvni zapis metody upva hlavni");
-            Console.WriteLine("Nová větev NOVA VETEV ");
+            Console.WriteLine("Nová větev NOVA VETEV  ");
 
             Console.WriteLine("nov branch 2");
         }
